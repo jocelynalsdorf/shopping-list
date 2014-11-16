f1d44e66a65bbd3ea985a1d4a056c9cb9763f997
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-16 15:33:06 +0700

    fixed append and form submit problem

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 
 $(document).ready(function() {
 	
-   $('button').click(function() {
-	var textValue = $("#list-text-box").val();
+    $('form').submit(function(event) {
+     event.preventDefault();
+	   var textValue = $("#list-text-box").val();
      if (textValue ==="") {
        alert("enter an item");
      }
      else {
-     	$("ul").append("<li class="shp-items"><i class="fa fa-square-o"></i><span>" + textValue + "</span><i class="fa fa-ban"></i></li>");
+     
+     	$("ul").append("<li class=\"shp-items\"><i class=\"fa fa-square-o\"></i><span>" + textValue + "</span><i class=\"fa fa-ban\"></i></li>");
 			}
 	});
+     
 
 });
 
